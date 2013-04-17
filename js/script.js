35cc96db4b4d447bc4eccac4d87eddc901ce59ca
Nikita <zubarets@gmail.com>
2013-04-17 12:52:40 +0300

    New changes
    1. Нужно убрать bold во всех местах, где используется шрифт Meta. Сейчас
    я вижу в сайдбаре некорректное отображение. В остальных местах вроде ок.
    2. Сеошный текст внизу на главной остался болдом.
    3. Подчеркивание города должно быть dashed (сейчас dotted), стрелочка
    должна тоже перекрашиваться и подчеркивание не должно убираться. Кстати
    dashed подчеркивания тоже нужно делать через border-bottom.
    4. Во всех ховерах сейчас border-bottom неправильного цвета, должно быть
    #efb2b2
    5. Кнопка найти у меня съехала немного влево.
    6. Попап с городами нужно закрывать по клику не только на крестик, а и
    любое другое место за рамками попапа.
    7. На текстовой странице подпись к картинке осталась без отступа сверху.
    8. На пейджере вперед и назад должны иметь ховер без плашки (см. папку с
    ролловерами)
    9. Для блоков Новые бренды и Популярные бренды в сайдбаре line-height:
    24px;
    10. На ссылке Информация о бренде осталось подчеркивание стрелки.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(function(){
 	
 	//WORK WITH DROPDOWN IN HEADER
+	$(document).click(function(){
+		$("#cityChooseMenu").hide();
+		console.log(true);
+	});
 	$("#cityChoose").on("click", function(e){
 		e.preventDefault();
-		$("#cityChooseMenu").toggle();
-	});
-	$("#cityChooseMenu").on("click", ".close", function(){
-		$("#cityChooseMenu").hide();
+		e.stopPropagation();
+		
+		$("#cityChooseMenu").show();
 	});
 	$("#cityChooseMenu").on("click", "a", function(e){
 		e.preventDefault();
+		e.stopPropagation();
+		
 		if(!$(this).hasClass("disabled")){
 			$("#cityChoose i").text( $(this).text() );
 			$("#cityChooseMenu").hide();
